991594fab8356ee4447f536ccf5d10c06566fd9f
huangjingxuanaa <237383368@qq.com>
2021-09-01 20:47:50 +0800

    12345663234

--- loop.sh
+++ loop.sh
@@ -6,7 +6,7 @@ for i in $(seq 1 $1)
 do 
    `sh ./push.sh`
    time=`date +'%G-%m-%d %H:%M:%S' -d '-1 days'`
-   timedatectl set-time "2021-09-10 20:44:22"
+   timedatectl set-time "$time"
 done
 
 
