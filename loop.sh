94a2b5723f34a7c8b31099b55231db01aaed9420
huangjingxuanaa <237383368@qq.com>
2024-06-07 14:41:27 +0800

    333456

--- loop.sh
+++ loop.sh
@@ -6,7 +6,8 @@ for i in $(seq 1 $1)
 do 
    `sh ./push.sh`
    time=`date +'%G-%m-%d %H:%M:%S' -d '-1 days'`
-   timedatectl set-time "$time"
+   #timedatectl set-time "$time"
+   timedatectl set-time "2022-09-04 22:39:20"
 done
 
 
--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
-git pull && /usr/bin/node  add.js && git commit -a -m '12345663234' && git push origin master
+git pull && /usr/bin/node  add.js && git commit -a -m '333456' && git push origin master
 
 
