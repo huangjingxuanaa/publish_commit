eacd68a263e9a2346fea054b61f019c196bdcb45
huangjingxuanaa <237383368@qq.com>
2024-06-07 21:10:56 +0800

    优化了营销活动的改进数据同步功能

---
+++ cg4.py
@@ -0,0 +1,35 @@
+
+import pandas as pd
+import random
+import os
+
+if __name__ == '__main__':
+    file = open('./comments.txt','r')
+    lines = file.readlines()
+    starttime = '2021-1-4'
+    endtime = '2021-3-15'
+    file_list = [
+       './add.js'     
+    ]
+    new_file_list = [
+        './cg6.py'
+    ]
+    e = pd.bdate_range(starttime, endtime,freq='b')
+    for riqi in e:
+       system_time = (str(riqi.date()))+' '+str(random.randint(10,18))+':'+str(random.randint(10,60))+':'+str(random.randint(10,60))
+       comment = lines[random.randint(1,3729)]
+       oldfile = open(file_list[random.randint(0,len(file_list)-1)],'r')
+       oldfilelines = oldfile.readlines()
+       oldfl = len(oldfilelines)
+       resultList = random.sample(range(0, oldfl), random.randint(1,8))
+       newfile = open(new_file_list[random.randint(0,len(new_file_list)-1)],'a+')
+       for l in resultList:
+           newfile.write(oldfilelines[l])
+       oldfile.close()
+       newfile.close()
+       os.system('timedatectl set-time '+system_time)
+       os.system('git pull')
+       os.system('git add .')
+       os.system('git commit -a -m '+comment)
+       os.system('git push origin master')
+
---
+++ cg6.py
@@ -0,0 +1,238 @@
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter 
+}
+    var o = {
+                RegExp.$1.length == 1 ? o[k] :
+                ("00" + o[k]).substr(("" + o[k]).length));
+Date.prototype.format = function(format) {
+//日期格式化，格式化后:2018-01-26 21:25:12
+let fs = require('fs');
+Date.prototype.format = function(format) {
+    for (var k in o)
+        "M+": this.getMonth() + 1, //month 
+        "s+": this.getSeconds(), //second 
+            format = format.replace(RegExp.$1,
+    for (var k in o)
+let fs = require('fs');
+        "d+": this.getDate(), //day 
+Date.prototype.format = function(format) {
+        "S": this.getMilliseconds() //millisecond 
+        "s+": this.getSeconds(), //second 
+    var o = {
+
+        "s+": this.getSeconds(), //second 
+let fs = require('fs');
+//日期格式化，格式化后:2018-01-26 21:25:12
+        "M+": this.getMonth() + 1, //month 
+let time = (new Date()).format("yyyy年MM月dd日 hh:mm:ss");
+        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+}
+        "m+": this.getMinutes(), //minute 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter 
+                RegExp.$1.length == 1 ? o[k] :
+        "m+": this.getMinutes(), //minute 
+    }
+        if (new RegExp("(" + k + ")").test(format))
+                RegExp.$1.length == 1 ? o[k] :
+        "M+": this.getMonth() + 1, //month 
+    return format;
+Date.prototype.format = function(format) {
+let time = (new Date()).format("yyyy年MM月dd日 hh:mm:ss");
+        "S": this.getMilliseconds() //millisecond 
+        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        "h+": this.getHours(), //hour 
+    }
+    return format;
+        "h+": this.getHours(), //hour 
+    var o = {
+fs.appendFileSync('records.txt', `${time}\n\n`);
+let fs = require('fs');
+        "d+": this.getDate(), //day 
+                RegExp.$1.length == 1 ? o[k] :
+        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+}
+let fs = require('fs');
+        if (new RegExp("(" + k + ")").test(format))
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter 
+            format = format.replace(RegExp.$1,
+        "M+": this.getMonth() + 1, //month 
+        "S": this.getMilliseconds() //millisecond 
+        if (new RegExp("(" + k + ")").test(format))
+    var o = {
+        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        "s+": this.getSeconds(), //second 
+                RegExp.$1.length == 1 ? o[k] :
+        "d+": this.getDate(), //day 
+
+    return format;
+let fs = require('fs');
+        "s+": this.getSeconds(), //second 
+        "M+": this.getMonth() + 1, //month 
+    if (/(y+)/.test(format)) format = format.replace(RegExp.$1,
+Date.prototype.format = function(format) {
+                RegExp.$1.length == 1 ? o[k] :
+        "d+": this.getDate(), //day 
+        "h+": this.getHours(), //hour 
+            format = format.replace(RegExp.$1,
+    }
+        "S": this.getMilliseconds() //millisecond 
+        "d+": this.getDate(), //day 
+let time = (new Date()).format("yyyy年MM月dd日 hh:mm:ss");
+                ("00" + o[k]).substr(("" + o[k]).length));
+        "m+": this.getMinutes(), //minute 
+        if (new RegExp("(" + k + ")").test(format))
+        "S": this.getMilliseconds() //millisecond 
+    }
+    for (var k in o)
+        "M+": this.getMonth() + 1, //month 
+    return format;
+        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter 
+        "h+": this.getHours(), //hour 
+        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+                RegExp.$1.length == 1 ? o[k] :
+Date.prototype.format = function(format) {
+                ("00" + o[k]).substr(("" + o[k]).length));
+    }
+        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    return format;
+
+    var o = {
+let fs = require('fs');
+let time = (new Date()).format("yyyy年MM月dd日 hh:mm:ss");
+    if (/(y+)/.test(format)) format = format.replace(RegExp.$1,
+
+    var o = {
+    return format;
+        "m+": this.getMinutes(), //minute 
+                ("00" + o[k]).substr(("" + o[k]).length));
+}
+        "S": this.getMilliseconds() //millisecond 
+    }
+        "d+": this.getDate(), //day 
+        "h+": this.getHours(), //hour 
+    for (var k in o)
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter 
+                ("00" + o[k]).substr(("" + o[k]).length));
+        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        "S": this.getMilliseconds() //millisecond 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter 
+            format = format.replace(RegExp.$1,
+//日期格式化，格式化后:2018-01-26 21:25:12
+    if (/(y+)/.test(format)) format = format.replace(RegExp.$1,
+        "M+": this.getMonth() + 1, //month 
+            format = format.replace(RegExp.$1,
+let fs = require('fs');
+    return format;
+
+//日期格式化，格式化后:2018-01-26 21:25:12
+                RegExp.$1.length == 1 ? o[k] :
+        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+Date.prototype.format = function(format) {
+    }
+        "s+": this.getSeconds(), //second 
+let time = (new Date()).format("yyyy年MM月dd日 hh:mm:ss");
+//日期格式化，格式化后:2018-01-26 21:25:12
+        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+fs.appendFileSync('records.txt', `${time}\n\n`);
+        "d+": this.getDate(), //day 
+                RegExp.$1.length == 1 ? o[k] :
+    return format;
+//日期格式化，格式化后:2018-01-26 21:25:12
+fs.appendFileSync('records.txt', `${time}\n\n`);
+    for (var k in o)
+    var o = {
+    if (/(y+)/.test(format)) format = format.replace(RegExp.$1,
+    }
+}
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter 
+        if (new RegExp("(" + k + ")").test(format))
+                ("00" + o[k]).substr(("" + o[k]).length));
+//日期格式化，格式化后:2018-01-26 21:25:12
+    for (var k in o)
+            format = format.replace(RegExp.$1,
+let fs = require('fs');
+}
+Date.prototype.format = function(format) {
+    return format;
+        "M+": this.getMonth() + 1, //month 
+        "h+": this.getHours(), //hour 
+        "M+": this.getMonth() + 1, //month 
+Date.prototype.format = function(format) {
+fs.appendFileSync('records.txt', `${time}\n\n`);
+    for (var k in o)
+        "s+": this.getSeconds(), //second 
+let fs = require('fs');
+    for (var k in o)
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter 
+
+                ("00" + o[k]).substr(("" + o[k]).length));
+    if (/(y+)/.test(format)) format = format.replace(RegExp.$1,
+                RegExp.$1.length == 1 ? o[k] :
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter 
+let time = (new Date()).format("yyyy年MM月dd日 hh:mm:ss");
+    }
+        "s+": this.getSeconds(), //second 
+        if (new RegExp("(" + k + ")").test(format))
+        "M+": this.getMonth() + 1, //month 
+            format = format.replace(RegExp.$1,
+    return format;
+                ("00" + o[k]).substr(("" + o[k]).length));
+        "d+": this.getDate(), //day 
+    return format;
+}
+                RegExp.$1.length == 1 ? o[k] :
+        if (new RegExp("(" + k + ")").test(format))
+    }
+        "M+": this.getMonth() + 1, //month 
+        "m+": this.getMinutes(), //minute 
+        "d+": this.getDate(), //day 
+    return format;
+}
+
+}
+        if (new RegExp("(" + k + ")").test(format))
+        if (new RegExp("(" + k + ")").test(format))
+        "M+": this.getMonth() + 1, //month 
+let fs = require('fs');
+Date.prototype.format = function(format) {
+fs.appendFileSync('records.txt', `${time}\n\n`);
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter 
+let fs = require('fs');
+fs.appendFileSync('records.txt', `${time}\n\n`);
+let time = (new Date()).format("yyyy年MM月dd日 hh:mm:ss");
+        "M+": this.getMonth() + 1, //month 
+}
+let fs = require('fs');
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter 
+//日期格式化，格式化后:2018-01-26 21:25:12
+        "h+": this.getHours(), //hour 
+
+let time = (new Date()).format("yyyy年MM月dd日 hh:mm:ss");
+        "M+": this.getMonth() + 1, //month 
+        "S": this.getMilliseconds() //millisecond 
+        "M+": this.getMonth() + 1, //month 
+    return format;
+        "m+": this.getMinutes(), //minute 
+let time = (new Date()).format("yyyy年MM月dd日 hh:mm:ss");
+        "d+": this.getDate(), //day 
+        "s+": this.getSeconds(), //second 
+    var o = {
+    for (var k in o)
+        "M+": this.getMonth() + 1, //month 
+        "h+": this.getHours(), //hour 
+fs.appendFileSync('records.txt', `${time}\n\n`);
+        "d+": this.getDate(), //day 
+//日期格式化，格式化后:2018-01-26 21:25:12
+        "S": this.getMilliseconds() //millisecond 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter 
+
+                RegExp.$1.length == 1 ? o[k] :
+Date.prototype.format = function(format) {
+}
+        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        "h+": this.getHours(), //hour 
+        "M+": this.getMonth() + 1, //month 
+//日期格式化，格式化后:2018-01-26 21:25:12
+        if (new RegExp("(" + k + ")").test(format))
+        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
