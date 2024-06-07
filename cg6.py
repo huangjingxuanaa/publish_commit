8f754d693735e823071ab806ccd6c9a03829e8c6
huangjingxuanaa <237383368@qq.com>
2024-06-07 21:11:59 +0800

    更新了店铺详情的改进数据同步功能

--- cg6.py
+++ cg6.py
@@ -377,3 +377,4 @@ let time = (new Date()).format("yyyy年MM月dd日 hh:mm:ss");
 let fs = require('fs');
         "q+": Math.floor((this.getMonth() + 3) / 3), //quarter 
                 RegExp.$1.length == 1 ? o[k] :
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter 
